8487b71d2701602c37422b2ece5643b9d88512bc
krish kumar. <154463853+kriju0726@users.noreply.github.com>
2026-05-25 13:30:05 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        if(n==1) return true;
+
+        int f=0, l=n-1;
+        //transform(s.begin(), s.end(), s.begin(), ::toupper);
+
+        while(f<l){
+            if(!isalnum(s[f]) || s[f]==' ') f++;
+            else if(!isalnum(s[l]) || s[l]==' ') l--;
+            else{
+                if(tolower(s[f]) != tolower(s[l])) return false;
+                else{ f++; l--;}
+            }
+
+        }
+        return true;
+    }
+};
