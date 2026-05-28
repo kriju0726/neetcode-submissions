34b576d9e74c92c35c9fa7c72607b693750d25e9
krish kumar. <154463853+kriju0726@users.noreply.github.com>
2026-05-28 17:46:29 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string res;
+        for(string s : strs)
+            res += to_string(s.size()) + '#' + s;
+
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j] != '#')
+                j++;
+
+            int len = stoi(s.substr(i, j-i));
+            j++;
+            string temp = s.substr(j, len);
+            res.push_back(temp);
+             
+            i = j+len;
+        }
+        return res;
+    }
+};
